2b04ebdbfcfe9e3ee8a1e5dfe377a55da3c7815e
Ben Hays <ben@kajabi.com>
2018-05-15 10:39:37 -0700

    Setup config, added sprockets, jquery, etc.

--- config.rb
+++ config.rb
@@ -1,7 +1,16 @@
-activate :autoprefixer do |prefix|
-  prefix.browsers = "last 2 versions"
-end
-
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
+
+activate :asset_hash
+activate :directory_indexes
+activate :sprockets
+
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+  activate :gzip
+  activate :autoprefixer do |config|
+    config.browsers = ['last 3 versions', 'Explorer >= 10']
+  end
+end
---
+++ source/javascripts/scripts_body.js
@@ -0,0 +1,1 @@
+//= require benzo/_tester
